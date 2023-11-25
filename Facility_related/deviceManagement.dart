9c2c068f4088916eedc2a40866c1bb6902f27113
Maker-Kata <114391493+Maker-Kata@users.noreply.github.com>
2023-11-25 18:08:56 +0100

    backend update

--- Facility_related/deviceManagement.dart
+++ Facility_related/deviceManagement.dart
@@ -1,23 +1,30 @@
 import '../Machine_related/machine.dart';
+import '../Patient_related/appointment.dart';
 
 class deviceManagement {
   List<Machine> devices = [];
+  List<Machine> availableDevices = [];
 
   deviceManagement() {
     this.devices = initDevicePark();
+    this.availableDevices = devices;
   }
 
   void plannedMachineMaintenance(Machine machine) {
     machine.inMaint = true;
+    var maintSlot = Appointment.Maint(machine);
   }
 
   void machineRepair(Machine machine) {
     machine.isBroken = true;
+    var repairSlot = Appointment.Repair(machine);
   }
 
   void machineMonitoring(Machine machine) {
-    if (machine.inMaint || machine.reserved || machine.isBroken)
+    if (machine.inMaint || machine.reserved || machine.isBroken) {
       machine.available = false;
+      availableDevices.remove(machine);
+    }
   }
 
   List<Machine> initDevicePark() {
