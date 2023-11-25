ebcebc188c0b182c45cd8896a56ffa7676f03778
Maker-Kata <114391493+Maker-Kata@users.noreply.github.com>
2023-11-25 19:14:06 +0100

    backend updates with prio

--- Patient_related/patient.dart
+++ Patient_related/patient.dart
@@ -1,26 +1,45 @@
 import 'dart:ffi';
 import 'appointment.dart';
 import 'treatment.dart';
-
-enum slots {
-  none,
-  morning,
-  daytime,
-  afterwork,
-}
+import '../Machine_related/conditions.dart';
 
 class Patient {
   late String name = '';
   late DateTime DoB; //date of birth
-  late Char gender; //M/F necessary for booking
+  late Char gender; //M/F necessary for room booking
+  late Condition condition;
   late Treatment treatment; //class defined in folder
   late bool
       assistance; //difficulty moving, needs assistance, consider while booking
   late bool inPatient; //is in the facility
   late bool largeBodied;
-  late int priority;
+  int priority = -1;
   late List<Appointment> pastAppointments;
   String preferredSlot = slots.none.toString();
 
-  Patient() {}
+  Patient() {
+    //...init with input data
+
+    this.treatment = Treatment(this.condition);
+    if (this.largeBodied)
+      treatment.machines.remove((element) => element.type == "VB");
+    // VitalBeam not compatible with largebodied patients
+
+    //prio based on condition probability and other factors to consider
+    switch (this.condition.probability) {
+      case >= 15:
+        this.priority = 3;
+        break;
+      case >= 10:
+        this.priority = 4;
+        break;
+      default:
+        this.priority = 5;
+        break;
+    }
+    if (this.inPatient)
+      priority--; //increase prio for inpatients above walk-ins
+    if (this.pastAppointments.any((element) => element.cancelled))
+      priority--; //if someone had a cancelled/missed appointment the prio increases
+  }
 }
--- Patient_related/treatment.dart
+++ Patient_related/treatment.dart
@@ -2,58 +2,54 @@ import '../Machine_related/conditions.dart';
 import '../Machine_related/machine.dart';
 import '../exec.dart';
 
-class Treatment{
+class Treatment {
   late String name;
-  List<Machine> machines =[];
-  late Duration duration; //treatment duration in minutes, can be used directly for booking a slot
-  int progress=0; // number of fractions already done
+  List<Machine> machines = [];
+  late Duration
+      duration; //treatment duration in minutes, can be used directly for booking a slot
+  int fractions = 0; // number of fractions to do
+  int progress = 0; // number of fractions already done
 
-Treatment(Condition condition)
-{
-  this.name=condition.name;
-    for (int i=0; i<condition.suitableMachines.length; i++)
-  {
-    for (int j=0; j<devMan.devices.length; j++)
-    if (condition.suitableMachines[i] == devMan.devices[j].type)
-    {
-      machines.add(devMan.devices[j]);
+  Treatment(Condition condition) {
+    this.name = condition.name;
+    for (int i = 0; i < condition.suitableMachines.length; i++) {
+      for (int j = 0; j < devMan.devices.length; j++)
+        if (condition.suitableMachines[i] == devMan.devices[j].type) {
+          machines.add(devMan.devices[j]);
+        }
+    }
+
+    switch (condition.rawName) {
+      case 'Craniospinal':
+        this.duration = Duration(minutes: 30);
+        break;
+      case 'Breast':
+        this.duration = Duration(minutes: 12);
+        break;
+      case 'Breast_Special':
+        this.duration = Duration(minutes: 20);
+        break;
+      case 'Head_and_Neck':
+        this.duration = Duration(minutes: 12);
+        break;
+      case 'Abdomen':
+        this.duration = Duration(minutes: 12);
+        break;
+      case 'Pelvis':
+        this.duration = Duration(minutes: 12);
+        break;
+      case 'Crane':
+        this.duration = Duration(minutes: 10);
+        break;
+      case 'Lung':
+        this.duration = Duration(minutes: 12);
+        break;
+      case 'Lung_Special':
+        this.duration = Duration(minutes: 15);
+        break;
+      case 'Whole_Brain':
+        this.duration = Duration(minutes: 10);
+        break;
     }
   }
-  
-  switch (condition.rawName)
-  {
-    case 'Craniospinal':
-      this.duration=Duration(minutes:30);
-      break;
-    case 'Breast':
-      this.duration=Duration(minutes:12); 
-      break;
-    case 'Breast_Special':      
-      this.duration=Duration(minutes:20); 
-      break;
-    case 'Head_and_Neck':      
-      this.duration=Duration(minutes:12); 
-      break;
-    case 'Abdomen':      
-      this.duration=Duration(minutes:12); 
-      break;
-    case 'Pelvis':      
-      this.duration=Duration(minutes:12); 
-      break;
-    case 'Crane':      
-      this.duration=Duration(minutes:10); 
-      break;
-    case 'Lung':      
-      this.duration=Duration(minutes:12); 
-      break;
-    case 'Lung_Special':       
-      this.duration=Duration(minutes:15); 
-      break;
-    case 'Whole_Brain':      
-      this.duration=Duration(minutes:10); 
-      break;     
-  }
-  
-}
-
 }
